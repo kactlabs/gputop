4555d9e5f5df102a2421bd8ebf602fbe5eda4053
Raja CSP Raman <raja.csp@gmail.com>
2025-12-25 13:56:33 -0500

    Updates for 2025-12-25

--- gputop.rb
+++ gputop.rb
@@ -1,8 +1,9 @@
 class Gputop < Formula
   desc "htop-like GPU monitor for Apple Silicon (Metal)"
   homepage "https://github.com/kactlabs/gputop"
-  url "https://github.com/kactlabs/gputop/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_ME"
+  url "https://codeload.github.com/kactlabs/gputop/tar.gz/refs/tags/v0.1.0"
+  sha256 "4af79b7bedf5514a8f5060a782a2b9e513b80a850c82a1b00225e0ded1324dff"
+  license "MIT"
 
   def install
     bin.install "gputop"
@@ -11,10 +12,9 @@ class Gputop < Formula
   def caveats
     <<~EOS
       gputop uses powermetrics and requires sudo.
-      Run it as:
 
+      Run it as:
         sudo gputop
     EOS
   end
 end
-
